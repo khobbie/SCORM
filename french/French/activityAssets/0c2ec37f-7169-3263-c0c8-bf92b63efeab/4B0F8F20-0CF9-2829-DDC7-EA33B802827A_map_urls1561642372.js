8e507dac8e1d2f4e746bedb195b7c4045310da5a
king kobby <ampahkwabena5@gmail.com>
2022-02-19 16:19:17 +0000

    finished the french translation for review

--- french/French/activityAssets/0c2ec37f-7169-3263-c0c8-bf92b63efeab/4B0F8F20-0CF9-2829-DDC7-EA33B802827A_map_urls1561642372.js
+++
@@ -1,4 +0,0 @@
-
-   var mapURLs = {"3f0e9738-d0fa-ac06-055b-b99b31e95dbf":"/d2l/le/content/6928/viewContent/7066/view","72e0a098-b5c7-fb1f-b61e-04bc40ac6053":"/d2l/le/content/6928/viewContent/7067/view","9465d11b-3644-a4a9-f015-a48c37070909":"/d2l/le/content/6928/viewContent/7068/view","abd6d25c-0a89-e546-1542-e3da8194e890":"/d2l/le/content/6928/viewContent/7069/view","afdd923a-abb5-0ab0-e74f-a0908c152eaf":"/d2l/le/content/6928/viewContent/7070/view","d19a6fda-b6bc-c95f-530c-6625dae633ac":"/d2l/le/content/6928/viewContent/7071/view"}
-   var mapPath = "/d2l/le/content/6928/viewContent/7072/view";
-   
--- french/French/activityAssets/72cd7493-a994-0f53-7104-187b0b391a8f/D6D6003F-5EE8-52C2-820F-71DA70F1342D_map_urls1561644441.js
+++
@@ -1,4 +0,0 @@
-
-   var mapURLs = {"0742b33e-f910-ab2c-c827-a544689c847a":"/d2l/le/content/6928/viewContent/7074/view","0c8abfb9-1939-b489-e632-156785e43c61":"/d2l/le/content/6928/viewContent/7075/view","1d51a1cd-b30c-dcb3-77ae-bcc9e26938b5":"/d2l/le/content/6928/viewContent/7076/view","277b0f97-7744-b020-926c-06a2a7a618f1":"/d2l/le/content/6928/viewContent/7077/view","db3d063c-a8e9-dffc-571b-e560fe9293d7":"/d2l/le/content/6928/viewContent/7078/view"}
-   var mapPath = "/d2l/le/content/6928/viewContent/7079/view";
-   
